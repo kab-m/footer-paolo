a9987cdbf5f250b745c2e5e71e8324c71defd1b2
Ridwaan Safarally <safarally.ridwaan@gmail.com>
2023-03-04 08:40:42 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {useState} from "react";
+import Header  from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+
+function App() {
+  return(
+    <div>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Header.scss"
+
+const Header= () => {
+    return(
+        <div className='navbar'>
+            NAVBAR
+        </div>
+    )
+}
+export default Header;
